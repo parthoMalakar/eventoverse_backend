974125f05dddb46ded486c721beddf1fa0b1b82c
Ratul-ratzz <ratul.officialid@gmail.com>
2025-07-05 11:52:14 +0530

    web

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  entry: "./app.js", // Pointing to your existing entry file
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"), // Output folder
+  },
+  target: "node", // Since it's a backend app
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader", // Optional: if you're using modern JS
+      },
+    ],
+  },
+};
