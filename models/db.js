14ab2cb5932ca8c6c3920190d3e3d59b625d6cba
Ratul <87857361+Ratul-ratzz@users.noreply.github.com>
2025-07-05 18:37:30 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,3 +1,4 @@
+// db/index.js
 const { Pool } = require("pg");
 require("dotenv").config();
 
@@ -7,24 +8,6 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-    ca: process.env.DB_SSL_CA,
-  },
-  // Additional connection settings for better reliability
-  connectionTimeoutMillis: 10000,
-  idleTimeoutMillis: 30000,
-  max: 20,
 });
 
-// Test the connection
-pool.on('connect', (client) => {
-  console.log('Connected to PostgreSQL database');
-});
-
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
-
-module.exports = pool; 
+module.exports = pool;
