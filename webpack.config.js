1417cdcbb75df94548f82b24ef71ddc5b6492cd6
Ratul-ratzz <ratul.officialid@gmail.com>
2025-07-05 11:57:05 +0530

    updated json

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader", // Optional: if you're using modern JS
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
